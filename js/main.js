20bd59963cc9ca83e35f2f0548b620b473de9337
Skvidos <79468362+Skvidos@users.noreply.github.com>
2022-04-11 21:44:04 +0600

    Added footer in `index.html` file

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(function () {
   $('.lang-switcher').on('click', function () {
     $('.lang-en').toggleClass('lang-en--active')
   });
+
+  $(".btn__arrow--3").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
 });
